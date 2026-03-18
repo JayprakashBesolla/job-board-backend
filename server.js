2aa31482c33471ec804d826469f90c83cfd93173
Jayaprakash Besolla <besollajayprakash@gmail.com>
2026-03-18 15:25:15 +0530

    add mongo env connection

--- server.js
+++ server.js
@@ -2,20 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const jobRoutes = require("./routes/jobRoutes"); // ✅ important
+const jobRoutes = require("./routes/jobRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// ✅ connect routes
+// ✅ MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected ✅"))
+  .catch((err) => console.log(err));
+
+// ✅ Routes
 app.use("/api/jobs", jobRoutes);
 
+// ✅ Test route
 app.get("/", (req, res) => {
   res.send("Your API is running 🚀");
 });
 
+// ✅ Port fix
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
